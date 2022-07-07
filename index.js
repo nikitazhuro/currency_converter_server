21a1f0866c1a4403d771be00d89a819d1849604e
= <=>
2022-07-07 21:39:25 +0300

    fix PORT

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const router = require('./Router');
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors())
 app.use('/api', router)
 
 const start = () => {
-    try {
-        app.listen( PORT, () => {
-            console.log(`Server started on ${PORT} PORT`)
-        }) 
-    } catch (e) {
-        console.log(e)
-    }
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server started on ${PORT} PORT`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
 }
 start()
