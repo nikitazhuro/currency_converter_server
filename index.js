421701eee21e7a73cd585e5d7906665168432a6e
= <=>
2022-07-10 17:16:45 +0300

    change link

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: 'https://zhuro-converter-client.web.app/',
+  credentials: true
+}
+
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 app.use('/api', router)
 
 const start = () => {
