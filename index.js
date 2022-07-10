8b0fe5e02106b3879c7901ddbe2e33e34c31090e
= <=>
2022-07-10 17:41:50 +0300

    fix last

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: 'https://zhuro-converter-client.web.app/',
-  credentials: true
+  origin: 'https://zhuro-converter-client.web.app',
+  credentials: true,
+  optionSuccessStatus: 200
 }
 
 app.use(express.json())
 app.use(cors(corsOptions))
 app.use('/api', router)
-
+optionSuccessStatus: 200
 const start = () => {
   try {
     app.listen(PORT, () => {
